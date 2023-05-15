54c02aa8fa2690f95dbefda51ae0943dc457433b
Juan SD <juanI.sosadiasmena@gmail.com>
2023-05-14 22:48:09 -0300

    Juan

---
+++ src/Library/Elfouno.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+namespace Library
+{
+
+    public class Elfouno
+    {
+        public int Vida { get; set; } = 100;
+        public string Nombre { get; set; }
+        public List<string> Items { get; set; }
+        public Elfouno(string nombre)//CONSTRUCTOR 
+        {
+            this.Nombre = nombre;
+        }
+        public void Atacar() // método para atacar
+        {
+            Console.WriteLine($"{Nombre} ha atacado a un enemigo.");
+        }
+
+        public void Curar() // método para curar (tendría que curar a otro, no a sí mismo)
+        {
+        }
+
+        public int ObtenerAtaque() // método que retorna el valor del ataque
+        {
+            int ataque = 10;
+            return ataque;
+        }
+
+        public int ObtenerDefensa() // método que retorna el valor de la defensa
+        {
+            int defensa = 10;
+            return defensa;
+        }
+
+        public void AgregarItem(string item) // método para agregar items a la lista
+        {
+            Items.Add(item);
+            Console.WriteLine($"{item} ha sido agregado al inventario de {Nombre}.");
+        }
+
+        public void RemoverItem(string item) // método para borrar items de la lista
+        {
+            if (Items.Contains(item))
+            {
+                Items.Remove(item);
+                Console.WriteLine($"{item} ha sido removido del inventario de {Nombre}.");
+            }
+            else
+            {
+                Console.WriteLine($"{item} no se encontraba en el inventario de {Nombre}.");
+            }
+        }
+    }
+}
