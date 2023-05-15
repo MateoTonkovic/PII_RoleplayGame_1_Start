ff4f23aee6f861e0da97dc534f85c69b1ebd1894
Juan SD <juanI.sosadiasmena@gmail.com>
2023-05-15 00:01:28 -0300

    Agregué Clase de Elementos

---
+++ src/Library/IElemento.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace Library 
+{
+    public class Elemento 
+    {
+        public int Ataque { get; set; }
+
+        public int Defensa { get; set; }
+
+        public int Curacion { get; set; }
+
+        public bool EsMagico { get; set; }
+    }
+}
