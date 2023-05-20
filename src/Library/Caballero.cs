b968fd37a0c16f943257bcf13e0cb6befe9e59a0
mariprieto01 <noel.prieto@correo.ucu.edu.uy>
2023-05-20 18:55:30 -0300

    Arquero y Caballero

--- src/Library/Caballero.cs
+++ src/Library/Caballero.cs
@@ -1,1 +1,16 @@
 // Enemigo
+using System;
+using System.Collections.Generic;
+
+namespace Library
+{
+    public class Caballero : Enemigo, IEnemigo
+    {
+        public Caballero(string nombre, int vida)
+        {
+            this.Nombre = nombre;
+            this.Vida = vida;
+        }
+
+    }
+}
