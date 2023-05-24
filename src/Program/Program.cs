4a24d5d2f2397612148b83cc0f0f157b49ac836b
MateoTonkovic <mateo@houlak.com>
2023-05-23 23:44:01 -0300

    Agregar encuentro en Program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -5,6 +5,31 @@ namespace Program
 {
     class Program
     {
-        static void Main(string[] args){ }
+        static void Main(string[] args)
+        {
+            Encuentro encuentro = new Encuentro();
+
+
+            Elfo elfo = new Elfo("Heroe legolas", 10);
+            Hacha hacha = new Hacha(90, 49);
+
+            elfo.AgregarElemento(hacha);
+
+            encuentro.AgregarHeroe(elfo);
+
+            Arquero arquero = new Arquero("Legolas", 10);
+            Caballero caballero = new Caballero("Aragorn", 10);
+
+            Lanza lanza = new Lanza(50, 1);
+
+            arquero.AgregarElemento(lanza);
+
+            caballero.AgregarElemento(lanza);
+
+            encuentro.AgregarEnemigo(arquero);
+            encuentro.AgregarEnemigo(caballero);
+
+            encuentro.DoEncounter();
+        }
     }
 }
